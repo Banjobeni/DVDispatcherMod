c4fc5fb41e6fcceca578d54699505dc57a9918e3
T3M8029 <102697658+T3M8029@users.noreply.github.com>
2024-12-12 22:59:45 +0100

    Merge parallel load tasks, fixes [issue #2](https://github.com/Banjobeni/DVDispatcherMod/issues/2)

--- DVDispatcherMod/TaskOverviewGenerator.cs
+++ DVDispatcherMod/TaskOverviewGenerator.cs
@@ -1,111 +1,328 @@
-﻿using System;
+﻿using DV.Logic.Job;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using DV.Logic.Job;
 using UnityEngine;
 
-namespace DVDispatcherMod {
-    public class TaskOverviewGenerator {
+namespace DVDispatcherMod
+{
+    public class TaskOverviewGenerator
+    {
         private static readonly FieldInfo StationControllerStationRangeField = typeof(StationController).GetField("stationRange", BindingFlags.Instance | BindingFlags.NonPublic);
 
         private readonly Dictionary<string, Color> _yardID2Color;
 
-        public TaskOverviewGenerator() {
+        public TaskOverviewGenerator()
+        {
             _yardID2Color = StationController.allStations.ToDictionary(s => s.stationInfo.YardID, s => s.stationInfo.StationColor);
         }
 
-        public string GetTaskOverview(Job job) {
-            var nearestYardID = StationController.allStations.OrderBy(s => ((StationJobGenerationRange)StationControllerStationRangeField.GetValue(s)).PlayerSqrDistanceFromStationCenter).FirstOrDefault()?.stationInfo.YardID;
+        public void DebugTaskOutput(Task task, ref int i, ref int carsNum, HashSet<string> cars, HashSet<string> warehouseLoadTypeCars)
+        {
+            //Debug.Log("task index: " + i);
+            i++;
+            var TaskData = task.GetTaskData();
+            //Debug.Log("task type: " + TaskData.type);
 
-            return GenerateTaskOverview(0, job.tasks.First(), nearestYardID);
-        }
+            if (TaskData.nestedTasks != null && TaskData.nestedTasks.Any())
+            {
+                //Debug.Log(TaskData.nestedTasks.Count.ToString() + " nested tasks");
 
-        private string GenerateTaskOverview(int indent, Task task, string nearestYardID) {
-            if (task.InstanceTaskType == TaskType.Parallel || task.InstanceTaskType == TaskType.Sequential) {
-                var taskData = task.GetTaskData();
-
-                //if (taskData.nestedTasks.Count == 1) {
-                //    GenerateTaskOverview(indent, taskData.nestedTasks[0], sb);
-                //} else {
-                //    AppendTaskLine(indent, task, sb);
-
-                return string.Join(Environment.NewLine, taskData.nestedTasks.Select(t => GenerateTaskOverview(indent + 1, t, nearestYardID)));
-                //}
-            } else {
-                return GetTaskString(indent, task, nearestYardID);
-            }
-        }
-
-        private string GetTaskString(int indent, Task task, string nearestYardID) {
-            var taskData = task.GetTaskData();
-
-            var stringBuilder = new StringBuilder();
-
-            if (task.InstanceTaskType == TaskType.Parallel) {
-                AppendIndented(indent, "Parallel", stringBuilder);
-            } else if (task.InstanceTaskType == TaskType.Sequential) {
-                AppendIndented(indent, "Sequential", stringBuilder);
-            } else if (task.InstanceTaskType == TaskType.Transport) {
-                AppendIndented(indent, $"Transport {FormatNumberOfCars(taskData.cars.Count)} from {FormatTrack(taskData.startTrack, nearestYardID)} to {FormatTrack(taskData.destinationTrack, nearestYardID)}", stringBuilder);
-            } else if (task.InstanceTaskType == TaskType.Warehouse) {
-                if (taskData.warehouseTaskType == WarehouseTaskType.Loading) {
-                    AppendIndented(indent, $"Load {FormatNumberOfCars(taskData.cars.Count)} at {FormatTrack(taskData.destinationTrack, nearestYardID)}", stringBuilder);
-                } else if (taskData.warehouseTaskType == WarehouseTaskType.Unloading) {
-                    AppendIndented(indent, $"Unload {FormatNumberOfCars(taskData.cars.Count)} at {FormatTrack(taskData.destinationTrack, nearestYardID)}", stringBuilder);
-                } else {
-                    AppendIndented(indent, "(unknown WarehouseTaskType)", stringBuilder);
+                foreach (Task NestedJobTask in TaskData.nestedTasks)
+                {
+                    DebugTaskOutput(NestedJobTask, ref i, ref carsNum, cars, warehouseLoadTypeCars);
                 }
-            } else {
-                AppendIndented(indent, "(unknown TaskType)", stringBuilder);
+            }
+            else
+            {
+                //Debug.Log("no nested tasks");
             }
 
-            if (task.state == TaskState.Done) {
-                return GetColoredString(Color.green, stringBuilder.ToString());
-            } else if (task.state == TaskState.Failed) {
-                return GetColoredString(Color.red, stringBuilder.ToString());
-            } else {
-                return stringBuilder.ToString();
+            if (TaskData.type == TaskType.Warehouse)
+            {
+                if (TaskData.warehouseTaskType == WarehouseTaskType.Loading)
+                {
+                    if (TaskData.cars != null && TaskData.cars.Any())
+                    {
+                        foreach (var car in TaskData.cars)
+                        {
+                            if (!warehouseLoadTypeCars.Contains(car.ID))
+                            {
+                                warehouseLoadTypeCars.Add(car.ID);
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (TaskData.cars != null && TaskData.cars.Any())
+            {
+                //Debug.Log((TaskData.cars.Count).ToString() + " cars");
+                if (TaskData.type != TaskType.Warehouse)
+                {
+                }
+                else
+                {
+                    //Debug.Log("start track: " + TaskData.startTrack.ID);
+                }
+                //Debug.Log("destination track: " + TaskData.destinationTrack.ID);
+                foreach (var car in TaskData.cars)
+                {
+                    if (!cars.Contains(car.ID))
+                    {
+                        cars.Add(car.ID);
+                        carsNum++;
+                    }
+                }
+            }
+            else
+            {
+                //Debug.Log("no cars");
             }
         }
 
-        private static string GetColoredString(Color color, string content) {
+        public void RecursivelyGetAllTasksData(Task task, ref int i, ref List<TaskData> JobTasks, ref List<int> indentList, int currentLevel = 0)
+        {
+            var TaskData = task.GetTaskData();
+            if (!JobTasks.Contains(TaskData))
+            {
+                //Debug.Log($"Adding {TaskData.type} task as {i} with indent level {currentLevel}");
+                JobTasks.Add(TaskData);
+                indentList.Add(currentLevel);
+                i++;
+            }
+            if (TaskData.nestedTasks != null && TaskData.nestedTasks.Any())
+            {
+                //Debug.Log($"that has {TaskData.nestedTasks.Count} nested tasks");
+                foreach (Task NestedJobTask in TaskData.nestedTasks)
+                {
+                    RecursivelyGetAllTasksData(NestedJobTask, ref i, ref JobTasks, ref indentList, currentLevel + 1);
+                }
+            }
+            else
+            {
+                //Debug.Log($"that has {0} nested tasks");
+            }
+        }
+
+        public List<TaskData> MergeTasks(List<TaskData> JobTasks, ref int i)
+        {
+            List<Car> mergedCars;
+            for (int index = 1; index < JobTasks.Count; index++)
+            {
+                var currentTask = JobTasks[index];
+                var previousTask = JobTasks[index - 1];
+                //Debug.Log("1st task type: " + previousTask.type);
+                //Debug.Log("2nd task type: " + currentTask.type);
+
+                if ((currentTask.type != TaskType.Sequential || currentTask.type != TaskType.Parallel) && (previousTask.type != TaskType.Sequential || previousTask.type != TaskType.Parallel))
+                {
+                    if (currentTask.type == TaskType.Warehouse && previousTask.type == TaskType.Warehouse)
+                    {
+                        //Debug.Log("1st task dest. track: " + previousTask.destinationTrack.ID);
+                        //Debug.Log("2nd task dest. track: " + currentTask.destinationTrack.ID);
+
+                        if (currentTask.warehouseTaskType == WarehouseTaskType.Loading &&
+                            previousTask.warehouseTaskType == WarehouseTaskType.Loading &&
+                            currentTask.destinationTrack == previousTask.destinationTrack)
+                        {
+                            //Debug.Log("Conditions met, tasks will merge");
+                            mergedCars = previousTask.cars.Union(currentTask.cars).ToList();
+                            TaskData mergedTaskData = new TaskData(
+                                currentTask.type,
+                                currentTask.state,
+                                currentTask.taskStartTime,
+                                currentTask.taskFinishTime,
+                                mergedCars,
+                                currentTask.startTrack,
+                                currentTask.destinationTrack,
+                                currentTask.warehouseTaskType,
+                                currentTask.cargoTypePerCar,
+                                currentTask.totalCargoAmount,
+                                null,
+                                currentTask.couplingRequiredAndNotDone,
+                                currentTask.anyHandbrakeRequiredAndNotDone
+                            );
+                            //Debug.Log($"Merged tasks of {previousTask.cars.Count} and {currentTask.cars.Count} to a new task of {mergedTaskData.cars.Count} cars.");
+
+                            JobTasks.RemoveAt(index - 1);
+                            JobTasks[index - 1] = mergedTaskData;
+                            i--;
+                        }
+                        else
+                        {
+                            //Debug.Log("Not merging, leaving task as are");
+                        }
+                    }
+                }
+                else
+                {
+                    //Debug.Log("Task is Sequential or Parallel, skipping");
+                }
+            }
+            return JobTasks;
+        }
+
+        public Tuple<List<TaskData>, List<int>> RebuiltTasksList(Job job)
+        {
+            int i = 0;
+            var JobTasks = new List<TaskData>();
+            var indentList = new List<int>();
+            //Debug.Log("index i is: " + i);
+            foreach (var jobTask in job.tasks)
+            {
+                RecursivelyGetAllTasksData(jobTask, ref i, ref JobTasks, ref indentList);
+                //Debug.Log("Flattened job task list (before merging):");
+                for (int i1 = 0; i1 < JobTasks.Count; i1++)
+                {
+                    TaskData task = JobTasks[i1];
+                    //Debug.Log(task.type + $" task, at list index {i1}");
+                }
+                i -= 1;
+                //Debug.Log("index i is: " + i + " list lenght is: " + JobTasks.Count);
+                JobTasks = MergeTasks(JobTasks, ref i);
+            }
+            int remergeCheck = JobTasks.Count();
+            while (remergeCheck > 6)
+            {
+                i = 0;
+                JobTasks = MergeTasks(JobTasks, ref i);
+                remergeCheck--;
+            }
+            //Debug.Log("Rebuilt job task list (after merging):");
+            foreach (var task in JobTasks)
+            {
+                //Debug.Log(task.type + "task");
+            }        
+            Tuple<List<TaskData>, List<int>> output = new Tuple<List<TaskData>, List<int>>(JobTasks, indentList);
+            return output;
+        }
+
+        public string GetTaskOverview(Job job)
+        {
+            var nearestYardID = StationController.allStations.OrderBy(s => ((StationJobGenerationRange)StationControllerStationRangeField.GetValue(s)).PlayerSqrDistanceFromStationCenter).FirstOrDefault()?.stationInfo.YardID;
+            int i = 0;
+            //var carsNum = 0;
+            int indent = 0;
+            var cars = new HashSet<string>();
+            var warehouseLoadTypeCars = new HashSet<string>();
+            //Debug.Log(job.ID);
+
+            foreach (var jobTask in job.tasks)
+            {
+                //DebugTaskOutput(jobTask, ref i, ref carsNum, cars, warehouseLoadTypeCars);
+            }
+            //Debug.Log("number of encountered cars: " + carsNum);
+            //Debug.Log(cars.ToString());
+            //Debug.Log("load number of cars" + warehouseLoadTypeCars.Count());
+            //return GenerateTaskOverview(0, job.tasks.First(), nearestYardID); - GTO probably not needed since recursive taskgoing is already done before in RecursivelyGetAllTasksData (?)
+            var TaskStringBuilder = new StringBuilder();
+            var outputStringBuilder = new StringBuilder();
+            foreach (var taskTaskData in RebuiltTasksList(job).Item1)
+            {
+                indent = RebuiltTasksList(job).Item2[i];
+                TaskStringBuilder.Clear();
+
+                if (taskTaskData.type == TaskType.Transport)
+                {
+                    AppendIndented(indent, $"Transport {FormatNumberOfCars(taskTaskData.cars.Count)} from {FormatTrack(taskTaskData.startTrack, nearestYardID)} to {FormatTrack(taskTaskData.destinationTrack, nearestYardID)}", TaskStringBuilder);
+                }
+                else if (taskTaskData.type == TaskType.Warehouse)
+                {
+                    if (taskTaskData.warehouseTaskType == WarehouseTaskType.Loading)
+                    {
+                        AppendIndented(indent, $"Load {FormatNumberOfCars(taskTaskData.cars.Count)} at {FormatTrack(taskTaskData.destinationTrack, nearestYardID)}", TaskStringBuilder);
+                    }
+                    else if (taskTaskData.warehouseTaskType == WarehouseTaskType.Unloading)
+                    {
+                        AppendIndented(indent, $"Unload {FormatNumberOfCars(taskTaskData.cars.Count)} at {FormatTrack(taskTaskData.destinationTrack, nearestYardID)}", TaskStringBuilder);
+                    }
+                    else
+                    {
+                        AppendIndented(indent, "(unknown WarehouseTaskType)", TaskStringBuilder);
+                    }
+                }
+                else if (taskTaskData.type == TaskType.Parallel)
+                {
+                    //Debug.Log("GTO-TaskType is parallel");
+                    //AppendIndented(indent, "Parallel", TaskStringBuilder);
+                }
+                else if (taskTaskData.type == TaskType.Sequential)
+                {
+                    //Debug.Log("GTO-TaskType is sequential");
+                    //AppendIndented(indent, "Sequential", TaskStringBuilder);
+                }
+                else
+                {
+                    AppendIndented(indent, "(unknown TaskType)", TaskStringBuilder);
+                }
+
+                if (taskTaskData.state == TaskState.Done)
+                {
+                    outputStringBuilder.Append(GetColoredString(Color.green, TaskStringBuilder.ToString()));
+                }
+                else if (taskTaskData.state == TaskState.Failed)
+                {
+                    outputStringBuilder.Append(GetColoredString(Color.red, TaskStringBuilder.ToString()));
+                }
+                else
+                {
+                    outputStringBuilder.Append(TaskStringBuilder.ToString());
+                }
+                i++;
+            }
+            //Debug.Log("final string is: " + Environment.NewLine + outputStringBuilder.ToString());
+            return outputStringBuilder.ToString();
+        }
+        private static string GetColoredString(Color color, string content)
+        {
             var colorString = GetHexColorComponent(color.r) + GetHexColorComponent(color.g) + GetHexColorComponent(color.b);
             return $"<color=#{colorString}>{content}</color>";
         }
 
-        private static string GetHexColorComponent(float colorComponent) {
+        private static string GetHexColorComponent(float colorComponent)
+        {
             return ((int)(255 * colorComponent)).ToString("X2");
         }
 
-        private string FormatTrack(Track track, string nearestYardID) {
-            if (track.ID.yardId == nearestYardID) {
+        private string FormatTrack(Track track, string nearestYardID)
+        {
+            if (track.ID.yardId == nearestYardID)
+            {
                 return Main.Settings.ShowFullTrackIDs ? track.ID.FullID : track.ID.TrackPartOnly;
             }
 
             var trackDisplayID = Main.Settings.ShowFullTrackIDs ? track.ID.FullID : track.ID.FullDisplayID;
-            if (_yardID2Color.TryGetValue(track.ID.yardId, out var color)) {
+            if (_yardID2Color.TryGetValue(track.ID.yardId, out var color))
+            {
                 return GetColoredString(color, trackDisplayID);
             }
 
             return trackDisplayID;
         }
 
-        private static string FormatNumberOfCars(int count) {
-            if (count == 1) {
+        private static string FormatNumberOfCars(int count)
+        {
+            if (count == 1)
+            {
                 return "1 car";
-            } else {
+            }
+            else
+            {
                 return count + " cars";
             }
         }
 
-        private static void AppendIndented(int indent, string value, StringBuilder sb) {
-            for (var i = 0; i < indent; i += 1) {
+        private static void AppendIndented(int indent, string value, StringBuilder sb)
+        {
+            for (var i = 0; i < indent; i += 1)
+            {
                 sb.Append("  ");
             }
             sb.Append("- ");
-            sb.Append(value);
+            sb.Append(value + Environment.NewLine);
         }
     }
 }
