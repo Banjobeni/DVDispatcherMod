9841ef6aa96faa0e47e60c0d62129d2a4df6fe89
T3M8029 <102697658+T3M8029@users.noreply.github.com>
2025-04-21 19:11:12 +0200

    Fix duplication of hint managers after fast travelling.

--- DVDispatcherMod/DispatcherHintManagers/DispatcherHintManager.cs
+++ DVDispatcherMod/DispatcherHintManagers/DispatcherHintManager.cs
@@ -4,55 +4,70 @@ using DVDispatcherMod.DispatcherHintShowers;
 using DVDispatcherMod.PlayerInteractionManagers;
 using JetBrains.Annotations;
 
-namespace DVDispatcherMod.DispatcherHintManagers {
-    public sealed class DispatcherHintManager : IDisposable {
+namespace DVDispatcherMod.DispatcherHintManagers
+{
+    public sealed class DispatcherHintManager : IDisposable
+    {
         private readonly IPlayerInteractionManager _playerInteractionManager;
         private readonly IDispatcherHintShower _dispatcherHintShower;
         private readonly TaskOverviewGenerator _taskOverviewGenerator;
-
         private int _counterValue;
 
-        public DispatcherHintManager([NotNull] IPlayerInteractionManager playerInteractionManager, [NotNull] IDispatcherHintShower dispatcherHintShower, TaskOverviewGenerator taskOverviewGenerator) {
+        public DispatcherHintManager([NotNull] IPlayerInteractionManager playerInteractionManager, [NotNull] IDispatcherHintShower dispatcherHintShower, TaskOverviewGenerator taskOverviewGenerator)
+        {
             _playerInteractionManager = playerInteractionManager;
             _dispatcherHintShower = dispatcherHintShower;
             _taskOverviewGenerator = taskOverviewGenerator;
-
             _playerInteractionManager.JobOfInterestChanged += HandleJobObInterestChanged;
         }
 
-        public void SetCounter(int counterValue) {
+        public void SetCounter(int counterValue)
+        {
             _counterValue = counterValue;
             UpdateDispatcherHint();
         }
 
-        private void HandleJobObInterestChanged() {
+        private void HandleJobObInterestChanged()
+        {
             UpdateDispatcherHint();
-
-            if (Main.Settings.EnableDebugLoggingOfJobStructure) {
+            if (Main.Settings.EnableDebugLoggingOfJobStructure)
+            {
                 var job = _playerInteractionManager.JobOfInterest;
-                if (job != null) {
+                if (job != null)
+                {
                     DebugOutputJobWriter.DebugOutputJob(job);
                 }
             }
         }
 
-        private void UpdateDispatcherHint() {
+        private void UpdateDispatcherHint()
+        {
             var currentHint = GetCurrentDispatcherHint();
             _dispatcherHintShower.SetDispatcherHint(currentHint);
         }
 
-        private DispatcherHint GetCurrentDispatcherHint() {
+        private DispatcherHint GetCurrentDispatcherHint()
+        {
             var job = _playerInteractionManager.JobOfInterest;
-            if (job != null) {
+            if (job != null)
+            {
                 return new JobDispatch(job, _taskOverviewGenerator).GetDispatcherHint(_counterValue);
-            } else {
-                return null;
             }
+            return null;
         }
 
-        public void Dispose() {
-            _dispatcherHintShower.SetDispatcherHint(null);
+        public void Dispose()
+        {
+            if (_playerInteractionManager != null)
+            {
+                _playerInteractionManager.JobOfInterestChanged -= HandleJobObInterestChanged;
+            }
+            _dispatcherHintShower?.SetDispatcherHint(null);
             _playerInteractionManager?.Dispose();
+            if (_dispatcherHintShower is IDisposable disposableShower)
+            {
+                disposableShower.Dispose();
+            }
         }
     }
 }
