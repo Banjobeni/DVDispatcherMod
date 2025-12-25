cf0eb28907db426893cacda0893923559b2780bf
Banjobeni <none@none>
2025-12-25 12:07:50 +0100

    Work around Unity's special null comparison getting in the way of the ?. operator

--- DVDispatcherMod/DispatcherHintShowers/DispatcherHintShower.cs
+++ DVDispatcherMod/DispatcherHintShowers/DispatcherHintShower.cs
@@ -2,11 +2,12 @@
 using DV.UIFramework;
 using DV.Utils;
 using DVDispatcherMod.DispatcherHints;
+using DVDispatcherMod.Extensions;
 using UnityEngine;
 
 namespace DVDispatcherMod.DispatcherHintShowers {
     public class DispatcherHintShower : IDispatcherHintShower {
-        private readonly Transform _attentionLineTransform;
+        private readonly GameObject _attentionLineGameObject;
         private readonly NotificationManager _notificationManager;
 
         private GameObject _notification;
@@ -15,8 +16,7 @@ namespace DVDispatcherMod.DispatcherHintShowers {
             _notificationManager = notificationManager;
 
             // transforms cannot be instantiated directly, they always live within a game object. thus we create a single (unnecessary) game object and keep it's transform
-            var transformGivingGameObject = new GameObject("ObjectForTransform");
-            _attentionLineTransform = transformGivingGameObject.transform;
+            _attentionLineGameObject = new GameObject("ObjectForTransform");
         }
 
         public void SetDispatcherHint(DispatcherHint dispatcherHintOrNull) {
@@ -36,8 +36,8 @@ namespace DVDispatcherMod.DispatcherHintShowers {
             if (attentionPoint == null || Main.Settings.ShowAttentionLine == false) {
                 return null;
             } else {
-                _attentionLineTransform.position = attentionPoint.Value;
-                return _attentionLineTransform;
+                _attentionLineGameObject.transform.position = attentionPoint.Value;
+                return _attentionLineGameObject.transform;
             }
         }
 
@@ -47,10 +47,7 @@ namespace DVDispatcherMod.DispatcherHintShowers {
                 _notification = null;
             }
 
-            var gameObject = _attentionLineTransform?.gameObject;
-            if (gameObject != null) {
-                Object.Destroy(gameObject);
-            }
+            _attentionLineGameObject.DestroyIfAlive();
         }
     }
 }
---
+++ DVDispatcherMod/Extensions/UnityObjectExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace DVDispatcherMod.Extensions {
+    public static class UnityObjectExtensions {
+        public static void DestroyIfAlive(this UnityEngine.Object obj) {
+            // unity implements an implicit conversion to bool that returns false if the unmanaged object has been destroyed
+            var isObjectAlive = (bool)obj;
+            if (isObjectAlive) {
+                UnityEngine.Object.Destroy(obj);
+            }
+        }
+    }
+}
